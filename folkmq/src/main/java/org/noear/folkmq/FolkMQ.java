2eacd057814855a8c54ca31fda154462e4e38762
noear <noear@live.cn>
2023-11-27 14:00:49 +0800

    添加 FolkMQ 主类

---
+++ folkmq/src/main/java/org/noear/folkmq/FolkMQ.java
@@ -0,0 +1,33 @@
+package org.noear.folkmq;
+
+import org.noear.folkmq.client.MqClient;
+import org.noear.folkmq.client.MqClientDefault;
+import org.noear.folkmq.server.MqServer;
+import org.noear.folkmq.server.MqServerDefault;
+
+/**
+ * @author noear
+ * @since 1.0
+ */
+public class FolkMQ {
+    /**
+     * 获取版本
+     */
+    public static String version() {
+        return "1.0.6";
+    }
+
+    /**
+     * 创建服务端
+     */
+    public static MqServer createServer() {
+        return new MqServerDefault();
+    }
+
+    /**
+     * 创建客户端
+     */
+    public static MqClient createClient(String serverUrl) {
+        return new MqClientDefault(serverUrl);
+    }
+}

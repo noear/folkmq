3cfa273d7d32658ffd10f2c9b8aa999df2dfbe82
noear <noear@live.cn>
2023-11-30 08:49:56 +0800

    调整压测代码

---
+++ folkmq-test/src/test/java/benchmark/ConsumerTest.java
@@ -0,0 +1,22 @@
+package benchmark;
+
+import org.noear.folkmq.FolkMQ;
+import org.noear.folkmq.client.MqClient;
+
+public class ConsumerTest {
+    public static void main(String[] args) throws Exception {
+
+        //客户端
+        MqClient client = FolkMQ.createClient("folkmq://127.0.0.1:18602?ak=folkmq&sk=YapLHTx19RlsEE16")
+                .connect();
+
+        //订阅
+        client.subscribe("hot", "a", message -> {
+            //System.out.println("::" + topic + " - " + message);
+        });
+
+        client.subscribe("test", "a", message -> {
+            //System.out.println("::" + topic + " - " + message);
+        });
+    }
+}
---
+++ folkmq-test/src/test/java/benchmark/ProducerTest.java
@@ -0,0 +1,25 @@
+package benchmark;
+
+import org.noear.folkmq.FolkMQ;
+import org.noear.folkmq.client.MqClient;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ProducerTest {
+    public static void main(String[] args) throws Exception {
+        //客户端
+        MqClient client = FolkMQ.createClient("folkmq://127.0.0.1:18602?ak=folkmq&sk=YapLHTx19RlsEE16")
+                .connect();
+
+        int count = 1000_000;
+
+        //发布预热
+        for (int i = 0; i < 100; i++) {
+            client.publish("hot", "hot-" + i).get();
+        }
+
+        for (int i = 0; i < count; i++) {
+            client.publish("test", "test-" + i);
+        }
+    }
+}

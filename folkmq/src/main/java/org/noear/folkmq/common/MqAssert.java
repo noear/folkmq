c105cbbdbda0dad4665a26ae6fef20e6f302e293
noear <noear@live.cn>
2024-02-22 21:08:22 +0800

    添加客户端相关参数校验

--- folkmq/src/main/java/org/noear/folkmq/common/MqAssert.java
+++ folkmq/src/main/java/org/noear/folkmq/common/MqAssert.java
@@ -9,22 +9,24 @@ package org.noear.folkmq.common;
 public class MqAssert {
     /**
      * 断言元信息相关的名字与值
-     * */
+     */
     public static void assertMeta(String str, String paramName) {
-        if (str.indexOf('?') >= 0) {
-            throw new IllegalArgumentException("Param '" + paramName + "' can't have symbols: '?'");
-        }
+        assertMetaSymbols(str, paramName, '?', "?");
+        assertMetaSymbols(str, paramName, '&', "&");
+        assertMetaSymbols(str, paramName, '=', "=");
+        assertMetaSymbols(str, paramName, '!', "!");
 
-        if (str.indexOf('&') >= 0) {
-            throw new IllegalArgumentException("Param '" + paramName + "' can't have symbols: '&'");
-        }
+        assertMetaSymbols(str, paramName, '@', "@");
+        assertMetaSymbols(str, paramName, '#', "#");
+        assertMetaSymbols(str, paramName, '$', "$");
+        assertMetaSymbols(str, paramName, '%', "%");
+        assertMetaSymbols(str, paramName, '^', "^");
+        assertMetaSymbols(str, paramName, '*', "*");
+    }
 
-        if (str.indexOf('=') >= 0) {
-            throw new IllegalArgumentException("Param '" + paramName + "' can't have symbols: '='");
-        }
-
-        if (str.indexOf('!') >= 0) {
-            throw new IllegalArgumentException("Param '" + paramName + "' can't have symbols: '!'");
+    public static void assertMetaSymbols(String str, String paramName, char c, String cS) {
+        if (str.indexOf(c) >= 0) {
+            throw new IllegalArgumentException("Param '" + paramName + "' can't have symbols: '" + cS + "'");
         }
     }
 }

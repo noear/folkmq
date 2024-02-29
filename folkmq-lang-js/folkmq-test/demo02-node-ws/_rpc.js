98c0f8b7967c3001003dba38e8bf74bc771a8002
noear <noear@live.cn>
2024-03-01 06:42:15 +0800

    调整测试端口

--- folkmq-lang-js/folkmq-test/demo02-node-ws/_rpc.js
+++ folkmq-lang-js/folkmq-test/demo02-node-ws/_rpc.js
@@ -3,19 +3,19 @@ const {SocketD} = require("@noear/socket.d");
 
 async function main() {
     //客户端1
-    const client1 =  await FolkMQ.createClient("folkmq:ws://127.0.0.1:18603")
+    const client1 =  await FolkMQ.createClient("folkmq:ws://127.0.0.1:18602")
         .nameAs("app1")
         .connect();
 
     const router = FolkMQ.newRouter(m=>m.getTag()).doOn("/test/hello",req=>{
-        req.respone(SocketD.newEntity("me to!"));
+        req.response(SocketD.newEntity("me to!"));
     });
     //客户端1监听
     client1.listen(router.consume.bind(router));
 
 
     //客户端2
-    let client2 =  await FolkMQ.createClient("folkmq:ws://127.0.0.1:18603")
+    let client2 =  await FolkMQ.createClient("folkmq:ws://127.0.0.1:18602")
         .nameAs("app2")
         .connect();
 
--- folkmq-lang-js/folkmq-test/demo02-node-ws/_tran.js
+++ folkmq-lang-js/folkmq-test/demo02-node-ws/_tran.js
@@ -1,7 +1,7 @@
 const {FolkMQ} = require("@noear/folkmq");
 
 async function main() {
-    const client = await FolkMQ.createClient("folkmq:ws://127.0.0.1:18603")
+    const client = await FolkMQ.createClient("folkmq:ws://127.0.0.1:18602")
         .nameAs("demoapp")
         .connect();
 

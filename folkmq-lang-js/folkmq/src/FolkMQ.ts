316369c868eb53f53e1d1552bcd683b24836d5e1
noear <noear@live.cn>
2024-05-16 17:27:10 +0800

    1.4.5

--- folkmq-lang-js/folkmq/src/FolkMQ.ts
+++ folkmq-lang-js/folkmq/src/FolkMQ.ts
@@ -28,7 +28,7 @@ export class FolkMQ {
      * 获取版本
      */
     static versionName(): string {
-        return "1.4.4";
+        return "1.4.5";
     }
 
     /**
--- folkmq-lang-js/folkmq/webpack.md
+++ folkmq-lang-js/folkmq/webpack.md
@@ -39,7 +39,7 @@ npm publish
 ```javascript
 
 /*!
- * FolkMQ v1.4.4
+ * FolkMQ v1.4.5
  * (c) 2023-2024 noear.org
  * Released under the LGPL-2.1 License.
  */

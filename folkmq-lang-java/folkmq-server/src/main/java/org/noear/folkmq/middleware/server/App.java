aaf5d575858b79ee2d3b8923e2e9c3fb6a7d8662
noear <noear@live.cn>
2024-05-28 23:23:36 +0800

    添加 folkmq-embedded 模块

--- folkmq-lang-java/folkmq-server/src/main/java/org/noear/folkmq/middleware/server/App.java
+++ folkmq-lang-java/folkmq-server/src/main/java/org/noear/folkmq/middleware/server/App.java
@@ -9,7 +9,7 @@ import org.noear.solon.Solon;
 public class App {
     public static void main(String[] args) {
         Solon.start(App.class, args, app -> {
-            app.get("/", ctx -> ctx.redirect("/folkmq/"));
+            app.get("/", ctx -> ctx.redirect("/folkmq/login"));
         });
     }
 }
